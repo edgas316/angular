582414116366c5ae2ec05702cca9eea2dd398a42
gaspaed <Edvin.Gasparian@verizonwireless.com>
2016-03-10 17:20:02 -0800

    end of day

--- app.js
+++ app.js
@@ -1,62 +1,129 @@
 // MODULE
-var angularApp = angular.module('angularApp', ['ngMessages','ngResource']);
+var angularApp = angular.module('angularApp', ['ngMessages','ngResource', 'ngRoute']);
 
+angularApp.config(function($routeProvider){
+    $routeProvider
+        .when('/', {
+            templateUrl: 'pages/main.html',
+            controller: 'mainController'
+        })
+        .when('/second', {
+            templateUrl: 'pages/second.html',
+            controller: 'secondController'
+        })
+        .when('/second/:num', {
+            templateUrl: 'pages/second.html',
+            controller: 'secondController'
+        })
+})
+
+
+angularApp.controller('secondController', ['$scope', '$log', '$routeParams', function($scope, $log, $routeParams){
+    $log.info('Second controller loaded!!!')
+    $scope.name = "Marine"
+    $scope.num = $routeParams.num || 'newpage'
+}]);
 // CONTROLLERS
-angularApp.controller('mainController', ['$scope', '$log', '$filter', '$resource', '$timeout', function ($scope, $log, $filter, $resource, $timeout) {
+angularApp.controller('mainController', ['$scope', '$log', '$filter', '$resource', '$timeout', '$http','$location', function ($scope, $log, $filter, $resource, $timeout, $http, $location) {
+    
+    $log.info($location.path())
+    $scope.thename = "Edwin"
+    
+    // ROUTING
+    
     $scope.name = "Edwin"
     
-    console.log($scope)
-    console.log($log )
-    
-    $log.log("hello")
-    $log.info('this is some info')
-    $log.warn('Warning!')
-    $log.debug('some debug information')
-    $log.error('this is an error')
-    
-    $scope.formattername = $filter('uppercase')($scope.name)
-    $log.info($scope.name)
-    $log.info($scope.formattername)
-    
-    console.log($resource)
-    
-    $timeout(function(){
-        $scope.name = 'Everybody'
-    }, 3000)
-    
-    $scope.handle = 'yourname'
-    $scope.lowercasehandle = function(){
-        return $filter('lowercase')($scope.handle)
-    }
-    
-    $scope.characters = 5
-    $scope.rules = [
-        {rulename: "Must be 5 characters"},
-        {rulename: "Must not be used elsewhere"},
-        {rulename: "Must be cool"}
-    ]
-    
-    console.log($scope.rules)
-    
-    var tb = document.getElementById("name")
-    
-    tb.addEventListener("keypress", function(event){
-        console.log(event)
-    })
-    
-    $scope.$watch('handle', function(newValue, oldValue){
-        console.info('Changed!')
-        console.log('Old: ' + oldValue)
-        console.log('New: ' + newValue)
-    })
-// =================================================
-    setTimeout(function(){
-        $scope.$apply(function(){
-            $scope.handle = "newteitterhandle"
-            console.log('Scope changed!')
-        })
-    }, 4000)
-// =================================================
+//    console.log($scope)
+//    console.log($log )
+//    
+//    $log.log("hello")
+//    $log.info('this is some info')
+//    $log.warn('Warning!')
+//    $log.debug('some debug information')
+//    $log.error('this is an error')
+//    
+//    $scope.formattername = $filter('uppercase')($scope.name)
+//    $log.info($scope.name)
+//    $log.info($scope.formattername)
+//    
+//    console.log($resource)
+//    
+//    $timeout(function(){
+//        $scope.name = 'Everybody'
+//    }, 3000)
+//    
+//    $scope.handle = 'yourname'
+//    $scope.lowercasehandle = function(){
+//        return $filter('lowercase')($scope.handle)
+//    }
+//    
+//    $scope.characters = 5
+//    $scope.rules = [
+//        {rulename: "Must be 5 characters"},
+//        {rulename: "Must not be used elsewhere"},
+//        {rulename: "Must be cool"}
+//    ]
+//    
+//    console.log($scope.rules)
+//    
+//    var tb = document.getElementById("name")
+//    
+//    tb.addEventListener("keypress", function(event){
+//        console.log(event)
+//    })
+//    
+//    $scope.$watch('handle', function(newValue, oldValue){
+//        console.info('Changed!')
+//        console.log('Old: ' + oldValue)
+//        console.log('New: ' + newValue)
+//    })
+//    
+//    $scope.alertClick = function(){
+//        alert("clicked")
+//    }
+//// =================================================
+//    setTimeout(function(){
+//        $scope.$apply(function(){
+//            $scope.handle = "newteitterhandle"
+//            console.log('Scope changed!')
+//        })
+//    }, 4000)
+//// =================================================
+//    
+//// =================================================  
+//// AJAX javascript native code
+//    var rulesrequest = new XMLHttpRequest()
+//    rulesrequest.onreadystatechange = function(){
+//        $scope.$apply(function(){
+//            if(rulesrequest.readyState == 4 && rulesrequest.status == 200){
+//                $scope.newrules = JSON.parse(rulesrequest.responseText)
+//            }
+//        })
+//    }
+//    rulesrequest.open("GET", "/api", true)
+//    rulesrequest.send()
+//// =================================================
+//// AJAX in angular way
+//    $http.get('/api')
+//        .success(function (result) {
+//            $scope.rules = result
+//        })
+//        .error(function(data, status){
+//            console.log(data)
+//        });
+//    
+//    $scope.newRule = ''
+//    $scope.addRule = function(){
+//        $http.post('/api', {newRule:$scope.newRule})
+//        .success(function(result){
+//            $scope.rules = result;
+//            $scope.newRule = ''
+//        })
+//        .error(function(data, status){
+//            console.log(data)
+//        })
+//    }
+
     
 }]);
 
